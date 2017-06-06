5d9ea3e5af296945d8da26e2a49e89b6fc5d09d7
Fangzhu-Tianji <zch793262201@163.com>
2017-06-06 23:05:03 +0800

    add

--- node/Vue/vue_demo1/src/App.vue
+++ node/Vue/vue_demo1/src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <h1 v-html="title"></h1>
+    <input v-model="newItem" @keyup.enter="onEnter" />
+    <ul>
+      <li v-for="item in items" v-bind:class="{red: item.isFunction}" v-on:click="toggleFinish('hello world')">
+        {{item.label}}
+      </li>
+    </ul>
     <img src="./assets/logo.png">
     <router-view></router-view>
   </div>
@@ -7,11 +14,42 @@
 
 <script>
 export default {
+  data (){
+    return {
+      title: '<span>hhaha</span>this is title',
+      items: [
+        {
+          label: 'coding',
+          isFunction: false
+        },
+        {
+          label: 'github',
+          isFunction: true
+        }
+      ],
+      newItem: ""
+    }
+  },
+  methods: {
+    toggleFinish: function(res){
+      console.log(res);
+    },
+    onEnter: function(){
+      this.items.push({
+        label: this.newItem,
+        isFunction: true
+      });
+      this.newItem = '';
+    }
+  },
   name: 'app'
 }
 </script>
 
 <style>
+.red {
+  color: red;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- node/Vue/vue_demo1/src/components/Hello.vue
+++ node/Vue/vue_demo1/src/components/Hello.vue
@@ -3,7 +3,7 @@
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
-      <li><a href="http://baidu.com" target="_blank">百度</a></li>
+      <li><a href="http://baidu.com" target="_blank">百1度</a></li>
       <li><a href="https://www.jd1.com" target="_blank">{{ jd }}</a></li>
       <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
